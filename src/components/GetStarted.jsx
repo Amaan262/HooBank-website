0900a9d90b56c5813642ce39f705bc20656dbb5b
Amaan Ahmad <Amaansaifi262@gmail.com>
2022-08-25 17:51:33 +0530

    initial commit

---
+++ src/components/GetStarted.jsx
@@ -0,0 +1,30 @@
+import styles from "../style";
+import { arrowUp } from "../assets";
+
+const GetStarted = () => {
+  return (
+    <>
+      <div
+        className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer`}
+      >
+        <div
+          className={`${styles.flexCenter} w-[100%] h-[100%] rounded-full bg-primary flex-col`}
+        >
+          <div className={`${styles.flexCenter} flex-row`}>
+            <p className="font-poppins font-medium text-[18px] leading-[23px]">
+              <span className="text-gradient">Get</span>
+            </p>
+              <img src={arrowUp} alt="arrow" className="w-[23px] h-[23px] object-contain" />
+          </div>
+            <p className="font-poppins font-medium text-[18px] leading-[23px]">
+              <span className="text-gradient">Started</span>
+            </p>
+
+            
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default GetStarted;
---
+++ src/components/index.js
@@ -0,0 +1,15 @@
+import Clients from "./Clients";
+import CTA from "./CTA";
+import Stats from "./Stats";
+import Footer from "./Footer";
+import Testimonials from "./Testimonials";
+import Hero from "./Hero";
+import Navbar from "./Navbar";
+import CardDeal from './CardDeal'
+import Billing from './Billing'
+import Business from "./Business";
+
+export{
+    Navbar,Billing,CardDeal,Business,Clients,CTA,Stats,Footer,Testimonials,Hero,
+
+}
